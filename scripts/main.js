9a4ba549d8b7c7db8aba09a1fa4a55ffeaf64240
MaxWou <mstoyanov18@mail.wou.edu.com>
2021-01-26 02:14:00 -0500

    added most of the existing files already.

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+$('.menu .item').tab();
+
+
+var what_page = document.getElementById('homepage');
+
+var captionLength = 0;
+var caption = '';
+
+$(document).ready(function () {
+    setInterval('cursorAnimation()', 600);
+    captionEl = $('#caption');
+
+    if the page is homepage it'll show my First Name
+    if (what_page != null)
+    {
+        caption = Maksim Stoyanov;
+    } else  else show Projects
+    {
+        caption = Projects;
+    }
+   
+    type();
+});
+
+
+function type() {
+    captionEl.html(caption.substr(0, captionLength++));
+    if (captionLength  caption.length + 1) {
+        setTimeout('type()', 220);
+    } else {
+        captionLength = 0;
+        caption = '';
+    }
+}
+
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity 0
+    }, 'fast', 'swing').animate({
+        opacity 1
+    }, 'fast', 'swing');
+}
+
+
+
+
